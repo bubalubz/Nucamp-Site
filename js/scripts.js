25f250f04d4a0dbd644dd71d3cd0bf9bdc6c1276
Jeffrey Strayhorn <thestrayhorns413@gmail.com>
2020-10-24 12:37:38 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,10 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
+    $("#reserveButton").click(function() {
+            $("#reserveCamp").modal("show");
+    });
+    $("#loginButton").click(function() {
+            $("#loginModal").modal("show");
+    });
 });
